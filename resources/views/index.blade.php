08de663a3378905db8e2481e02f82c2a671b6704
cristian-santos <cristiansantosti@gmail.com>
2022-07-28 23:43:38 -0300

    concluir tarefa

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link href="./assets/css/style.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/d093a04ba7.js" crossorigin="anonymous"></script>
     <title>To do list</title>
 </head>
 
@@ -40,22 +41,27 @@
                     <div class="row mb-3 d-flex align-items-center">
                         <div class="col-sm-9">
                            {{ $item->tarefa }}
-                        <hr>
+                            <hr>
                         </div>
+
                         <div class="col-sm-3">
-                        <a href="#" style="text-decoration: none">
-                            <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                                Concluir
-                            </button>
-                        </a>
+                            @if($item->status == 0)
+                            <a href="{{ route('tarefa.concluir', $item->id)}}" style="text-decoration: none">
+                                <button type="button" class="btn btn-success" title="Concluír tarefa">
+                                    <i class="fa-solid fa-check"></i>
+                                </button>
+                            </a>
+                           
+                            <a href="#" style="text-decoration: none">
+                                <button class="btn btn-primary" title="Editar tarefa"><i class="fa-solid fa-pen-to-square"></i></button>
+                            </a>
+                            @else
+                                <button class="btn btn-dark" disabled>Tarefa concluída</button>
+                            @endif
 
-                        <a href="#" style="text-decoration: none">
-                            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Editar</button>
-                        </a>
-
-                        <a href="{{ route('tarefa.destroy', $item->id)}}">
-                            <button class="btn btn-danger">Deletar</button>
-                        </a>
+                            <a href="{{ route('tarefa.destroy', $item->id)}}">
+                                <button class="btn btn-danger" title="Excluir tarefa"><i class="fa-solid fa-trash"></i></button>
+                            </a>
                         </div>
                     </div>
                     @endforeach
